e7644ad1d5873dfa581dcf5310f3e8d12fad3f86
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-20 18:05:20 -0500

    Refactor parameters for the file based trie_builder to accept a file name instead of the whole path.

--- substring_matcher/trie_builders.py
+++ substring_matcher/trie_builders.py
@@ -1,23 +1,24 @@
 import os
 
-from trie import Trie
+from substring_matcher.trie import Trie
 
 
-def build_trie_from_file(file_path: str) -> Trie:
+def build_trie_from_file(file_name: str) -> Trie:
     """
     Retrieves the keywords file, iterates through the keywords,
     then removes the newline from each keyword and adds it to the Trie.
 
-    The file_path should be a path that starts at the root. For example,
-    the file_path for the keywords.txt file would be 'substring_matcher/data/keywords.txt'.
+    The file_name should be the name of the file stored inside of the data folder,
+    which resides inside of the substring_matcher app.
+    Its name must contain the word 'keywords.'
     """
 
-    if not isinstance(file_path, str):
+    if not isinstance(file_name, str):
         raise TypeError
 
     trie: Trie = Trie()
     working_directory: str = os.getcwd()
-    keywords_file_path: str = f"{working_directory}/{file_path}"
+    keywords_file_path: str = f"{working_directory}/substring_matcher/data/{file_name}"
 
     with open(keywords_file_path, 'r', encoding='utf-8') as keywords_file:
         for keyword in keywords_file:
