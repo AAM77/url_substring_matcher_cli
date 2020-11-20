da39b5d37bdccf359fb3801142632c2f74f36ebd
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-19 23:00:06 -0500

    Add a parameter to the build_trie_from_file function to accept a file path.

--- substring_matcher/determine_runtime.py
+++ substring_matcher/determine_runtime.py
@@ -17,7 +17,7 @@ def determine_average_runtime_for_matching_keywords(url: str) -> int:
     """
 
     # build the trie
-    new_trie = build_trie_from_file()
+    new_trie = build_trie_from_file('substring_matcher/data/keywords.txt')
     print(new_trie.find_matching_substrings(url))
 
     # run the method a specified number of times.
--- substring_matcher/trie_builders.py
+++ substring_matcher/trie_builders.py
@@ -3,7 +3,7 @@ import os
 from trie import Trie
 
 
-def build_trie_from_file() -> Trie:
+def build_trie_from_file(file_path: str) -> Trie:
     """
     Retrieves the keywords file, iterates through the keywords,
     then removes the newline from each keyword and adds it to the Trie.
@@ -11,7 +11,7 @@ def build_trie_from_file() -> Trie:
 
     trie: Trie = Trie()
     working_directory: str = os.getcwd()
-    keywords_file_path: str = f"{working_directory}/substring_matcher/data/keywords.txt"
+    keywords_file_path: str = f"{working_directory}/{file_path}"
 
     with open(keywords_file_path, 'r', encoding='utf-8') as keywords_file:
         for keyword in keywords_file:
