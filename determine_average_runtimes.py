b78f0a7659aa975ccf3bcb1f65c7911011ad806f
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-22 22:02:38 -0500

    Refactor determine average runtimes file to use the Triebuilder and renamed test file for keywords.

--- determine_average_runtimes.py
+++ determine_average_runtimes.py
@@ -7,7 +7,7 @@ from substring_matcher.constants import (
     TWO_THOUSAND_CHARS_URL,
     ELEVEN_THOUSAND_CHARS_URL
 )
-from substring_matcher.trie_builders import build_trie_from_file
+from substring_matcher.trie_builder import TrieBuilder
 
 
 def determine_average_runtime_for_matching_keywords(url: str) -> int:
@@ -16,10 +16,11 @@ def determine_average_runtime_for_matching_keywords(url: str) -> int:
     to iterate through a url and retrieve the matching
     keywords using the find_matching_substrings method.
     """
-
+    trie_builder = TrieBuilder()
+    trie_builder.file_name = 'runtime_test_keywords.txt'
     # build the trie
-    new_trie = build_trie_from_file('runtime_test_keywords.txt')
-    print(new_trie.find_matching_substrings(url))
+    new_trie = trie_builder.build_trie_from_file()[0]
+    # print(new_trie.find_matching_substrings(url))
 
     # run the method a specified number of times.
     # collect the runtime from each method call.
@@ -39,20 +40,84 @@ def determine_average_runtime_for_matching_keywords(url: str) -> int:
     return average_runtime
 
 
-average_runtime_short = determine_average_runtime_for_matching_keywords(
-    SHORTEST_URL)
+def print_average_runtimes():
+    average_runtime_short = determine_average_runtime_for_matching_keywords(
+        SHORTEST_URL)
 
-average_runtime_medium = determine_average_runtime_for_matching_keywords(
-    THREE_HUNDRED_CHARS_URL)
+    average_runtime_medium = determine_average_runtime_for_matching_keywords(
+        THREE_HUNDRED_CHARS_URL)
 
-average_runtime_long = determine_average_runtime_for_matching_keywords(
-    TWO_THOUSAND_CHARS_URL)
+    average_runtime_long = determine_average_runtime_for_matching_keywords(
+        TWO_THOUSAND_CHARS_URL)
 
-average_runtime_super_long = determine_average_runtime_for_matching_keywords(
-    ELEVEN_THOUSAND_CHARS_URL)
+    average_runtime_super_long = determine_average_runtime_for_matching_keywords(
+        ELEVEN_THOUSAND_CHARS_URL)
 
-print(f"The Average Runtime for a short url is: {average_runtime_short} ms")
-print(f"The Average Runtime for a medium url is: {average_runtime_medium} ms")
-print(f"The Average Runtime for a long url is: {average_runtime_long} ms")
-print(
-    f"The Average Runtime for a super long url is: {average_runtime_super_long} ms")
+    print(
+        f"The Average Runtime for a short url is: {average_runtime_short} ms")
+    print(
+        f"The Average Runtime for a medium url is: {average_runtime_medium} ms")
+    print(f"The Average Runtime for a long url is: {average_runtime_long} ms")
+    print(
+        f"The Average Runtime for a super long url is: {average_runtime_super_long} ms")
+
+
+print_average_runtimes()
+print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
+
+# print_average_runtimes()
+# print("\n-------------------------------------\n")
